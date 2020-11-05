b8cc9f9e1f4e078cb899c1d405d8b3b43fcf9942
ingesolvoll <inge@semion.ca>
2020-11-05 20:52:33 +0100

    Deploy config

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject baking-soda "0.2.5"
+(defproject org.semion/baking-soda "0.2.5"
   :description "baking-soda is an interface between clojurescript's reagent
                 and reactstrap (i.e., bootstrap 4 react components)"
   :url "https://github.com/gadfly361/baking-soda"
@@ -7,6 +7,12 @@
         :url  "https://github.com/gadfly361/baking-soda"}
 
   :min-lein-version "2.5.3"
+
+  :plugins [[s3-wagon-private "1.3.4"]]
+  :repositories [["semion" {:url           "s3p://semion-repos/releases/"
+                            :username      :env/AWS_SECRET_ACCESS_KEY
+                            :passphrase    :env/AWS_ACCESS_KEY_ID
+                            :sign-releases false}]]
 
   :dependencies
   [[org.clojure/clojure "1.10.1"]
