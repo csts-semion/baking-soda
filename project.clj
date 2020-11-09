23867a77be1f1de667f2967f8d3ac817ad85d611
zalky <zalan.k@gmail.com>
2020-11-09 11:05:47 -0500

    Update semion repo def

--- project.clj
+++ project.clj
@@ -9,10 +9,8 @@
   :min-lein-version "2.5.3"
 
   :plugins [[s3-wagon-private "1.3.4"]]
-  :repositories [["semion" {:url           "s3p://semion-repos/releases/"
-                            :username      :env/AWS_SECRET_ACCESS_KEY
-                            :passphrase    :env/AWS_ACCESS_KEY_ID
-                            :sign-releases false}]]
+  :repositories [["semion" {:url     "s3p://semion-repos/web/"
+                            :no-auth true}]]
 
   :dependencies
   [[org.clojure/clojure "1.10.1"]
